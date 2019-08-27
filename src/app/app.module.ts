a94b28e5a2174f90f3c3f3f31d4192904f586377
Vitor <dev_vitorsouza@hotmail.com>
2019-08-27 14:47:22 -0300

    css global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    InputTextModule
     
   ],
   providers: [],
