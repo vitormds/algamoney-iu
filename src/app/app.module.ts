8b5ef25d626c85bdbe1b27d71c1ee3bef3fb2f33
Vitor <dev_vitorsouza@hotmail.com>
2019-08-30 00:22:23 -0300

    10.4. Adicionando o formulário de pesquisa de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
+ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {InputTextModule} from 'primeng/components/inputtext/inputtext';
+import {ButtonModule} from 'primeng/components/button/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    InputTextModule
-    
+    InputTextModule,
+    ButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
